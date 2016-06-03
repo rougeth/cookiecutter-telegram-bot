4d614a96fd44301c9b56d56339021ca77151324b
Marco Rougeth <marco@rougeth.com>
2016-06-03 00:19:42 -0300

    Added basic example

---
+++ {{ cookiecutter.bot_name|lower|replace(' ', '_') }}/{{ cookiecutter.bot_name|lower|replace(' ','_') }}.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+
+from telebot import TeleBot
+
+from exceptions import TokenNotDefined
+
+
+TOKEN = os.environ.get('TOKEN')
+if not TOKEN:
+    raise TokenNotDefined('TOKEN must be defined')
+
+bot = TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Welcome to {{ cookiecutter.bot_name | title }} Bot')
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling()
